4a4cd29f67c8fdd7f9f72a2da8e5beedcd9962dd
akif00 <akifmohammed0015@gmail.com>
2022-06-02 15:31:55 +0530

    Resolved Print Errors

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
-const d = new Date();
-const date=[d.getFullYear(),d.getMonth(),d.getDate()].join('');
-console.log(date);
+var d = new Date();
+var date = [d.getFullYear(), d.getMonth() + 1, d.getDate()].join("");
+// console.log(date);
